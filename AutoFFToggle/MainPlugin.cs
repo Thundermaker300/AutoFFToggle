0bcb7a32f12628613c90bbd19278aa482ed07c70
Thunder <thundermaker300@gmail.com>
2023-09-02 14:22:40 -0400

    Version

--- AutoFFToggle/MainPlugin.cs
+++ AutoFFToggle/MainPlugin.cs
@@ -13,7 +13,7 @@ namespace AutoFFToggle
     {
         public override string Name => "AutoFFToggle";
         public override string Author => "Thunder (originally Kognity)";
-        public override Version Version => new(2, 0, 2);
+        public override Version Version => new(2, 0, 3);
         public override Version RequiredExiledVersion => new(7, 0, 0);
         public override PluginPriority Priority => PluginPriority.Low;
 
